8eda3ebea882b19b773cbdd1eab5b31d7ba1324c
Corbin Petersen <corbin.petersen@marriottschool.byu.edu>
2017-12-13 17:13:16 -0700

    Add project files.

---
+++ 403_Project_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_403_Project_2.MvcApplication" Language="C#" %>
---
+++ 403_Project_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_403_Project_2.Startup))]
+namespace _403_Project_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
